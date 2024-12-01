ccc89fd97578e3154a2aac7a8683f6d4a3dde3e5
yarivtch <yarivtch@gmail.com>
2024-12-01 19:00:25 +0200

    הוספת README

--- app/src/androidTest/java/com/example/objectdetection/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/objectdetection/MainActivityInstrumentedTest.kt
@@ -1,1 +1,25 @@
- 
+@RunWith(AndroidJUnit4::class)
+class MainActivityInstrumentedTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testCameraPermission() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val permission = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.CAMERA
+        )
+        // בדיקה שההרשאה התקבלה
+        assertEquals(PackageManager.PERMISSION_GRANTED, permission)
+    }
+
+    @Test
+    fun testUIElements() {
+        onView(withId(R.id.startDetectionButton))
+            .check(matches(isDisplayed()))
+        
+        onView(withId(R.id.objectNameInput))
+            .check(matches(isDisplayed()))
+    }
+} 
